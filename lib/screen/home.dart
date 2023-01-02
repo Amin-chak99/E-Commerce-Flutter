fa9e0ceb31b3a806b32226b8f0310826b0109a9a
Amin-chak99 <76822547+Amin-chak99@users.noreply.github.com>
2023-01-03 00:01:13 +0100

    Getx

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,4 +1,5 @@
 import 'package:badges/badges.dart';
+import 'package:eshop/screen/FournisseurPage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -23,8 +24,7 @@ class HomePage extends GetView<ProductController> {
           IconButton(
             icon: Icon(Icons.login_outlined ,color: Color.fromRGBO(255, 122, 51, 10),),
             onPressed: () {
-              Navigator.of(context).pop();
-              Navigator.pushNamed(context,'/Fournisseur');
+              Get.to(()=> FournisseurPage());
             },
           ),
           Container(
@@ -173,106 +173,6 @@ class HomePage extends GetView<ProductController> {
 
     );})
       )
-    );
-  }
-
-  _buildAddEditEmployeeView({String? text, int? addEditFlag, String? docId}) {
-    Get.bottomSheet(
-      Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(
-            topRight: Radius.circular(16),
-            topLeft: Radius.circular(16),
-          ),
-          color: Color(0xff1E2746),
-        ),
-        child: Padding(
-          padding:
-          const EdgeInsets.only(left: 16, right: 16, top: 16, bottom: 16),
-          child: Form(
-            key: controller.formKey,
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '${text} Employee',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'Name',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                    ),
-                    controller: controller.nameController,
-                    validator: (value) {
-                      return controller.validateName(value!);
-                    },
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                    keyboardType: TextInputType.multiline,
-                    decoration: InputDecoration(
-                      hintText: 'Address',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                    ),
-                    controller: controller.addressController,
-                    validator: (value) {
-                      return controller.validateAddress(value!);
-                    },
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  ConstrainedBox(
-                    constraints: BoxConstraints.tightFor(
-                        width: Get.context!.width, height: 45),
-                    child: ElevatedButton(
-                      child: Text(
-                        text!,
-                        style: TextStyle(color: Colors.black, fontSize: 16),
-                      ),
-                      onPressed: () {
-                        controller.saveUpdateEmployee(
-                            controller.nameController.text,
-                            controller.addressController.text,
-                            docId!,
-                            addEditFlag!);
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-
-  displayDeleteDialog(String docId) {
-    Get.defaultDialog(
-      title: "Delete Employee",
-      titleStyle: TextStyle(fontSize: 20),
-      middleText: 'Are you sure to delete employee ?',
-      textCancel: "Cancel",
-      textConfirm: "Confirm",
-      confirmTextColor: Colors.black,
-      onCancel: () {},
-      onConfirm: () {
-        controller.deleteData(docId);
-      },
     );
   }
 }
